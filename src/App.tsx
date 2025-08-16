6fc242a16dc88d238b4ac734b6adc886bec5ac43
Deepak Kumar <125119545+deepak-077@users.noreply.github.com>
2025-08-16 22:35:59 +0530

    added login functionality and directing to Instagram homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,81 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Home';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+ const [username,SetUsername] = useState('');
+ const [password,setPassword] = useState('');
+ const [loggedin,setLoggedin] = useState(false);
+
+function isLoggedin(){
+
+  if(username == 'alpha' && password=='delta'){
+    setLoggedin(true);
+      
+  }
+  else{
+      alert(" error : incorrect username or password ")
+  }
+}
+  if(loggedin){
+    return <Home/>
+
+  } 
 
   return (
-    <div className='flex flex-col justify-center items-center bg-black h-screen '>
+    <div className='flex flex-col justify-center items-center bg-black min-h-screen  '>
       
       {/* header */}
-      <main className=' flex justify-center items-center w-[1306px] h-[555px] '>
-        <article className='flex w-[935px] h-[523px]  mt-[32px] mx-[186px] pb-[32px]'>
+      <main className=' flex justify-center items-center w-full max-w-[1306px] h-[555px] '>
+        <article className='flex  flex-col md:flex-row items-center w-full max-w-[935px] h-auto  mt-0 mx-0  xs:mt-8 md:mx-4  lg:mx-[186px]  pb-[32px]'>
 
-          {/* background image */}
-      <div className='w-[585px] h-[491px] flex justify-center'> 
+          {/* image */}
+      <div className='w-[585px] h-[491px] flex justify-center hidden md:block'> 
         <img className='w-[517px] h-[450px] ml-[-55px] ' src="landing.png" alt="" />
       </div>
 
       {/* credentials div */}
-      <div className='w-[350px] h-[480px] mt-[12px]'>
+      <div className='w-full min-w-[250px] max-w-[350px] h-[480px] mt-[12px] flex flex-col justify-center items-center mx-auto'>
         
-        <div className='w-[350px] h-[408px] py-[10px] mb-[10px] flex flex-col justify-center items-center'>
+        <div className='w-full min-w-[250px] max-w-[350px] h-[408px] py-[10px] mb-[10px] flex flex-col justify-center items-center'>
 
           <img className='w-[175px] h-[51px]' src='Instagram white.svg'></img>
 
-          <div className='w-[350px] h-[223px] flex flex-col items-center gap-2 mt-[24px]'> 
+          <div className='w-full min-w-[250px] max-w-[350px] h-[223px] flex flex-col items-center gap-2 mt-0 sm:mt-6'> 
 
             {/* username field */}
-            <div className='w-[270px] h-[38px] border-[#262626] border-[0.5px] rounded-sm '>
-              <input className='w-[260px] h-[36px] pt-[9px] pb-[7px] pl-[8px] text-white text-[12px]' type="text" placeholder='Phone number, username or email address' />
+            <div className='w-full max-w-[270px] min-w-[187px] h-[38px] border-[#262626] border-[0.5px] rounded-sm '>
+              <input id='username' className='w-full max-w-[260px] min-w-[183px] h-[36px] pt-[9px] pb-[7px] pl-[8px] text-white text-[12px] focus:outline-none' type="text" placeholder='Phone number, username or email address'
+              value={username}
+              onChange={(e)=>SetUsername(e.target.value)}/>
             </div>
 
             {/* password field */}
-            <div className='w-[270px] h-[38px] border-[#262626] border-[0.5px] rounded-sm '>
-              <input className='w-[260px] h-[36px] pt-[9px] pb-[7px] pl-[8px] text-white text-[12px]' type="text" placeholder='Password' />
+            <div className='w-full max-w-[270px] min-w-[187px] h-[38px] border-[#262626] border-[0.5px] rounded-sm '>
+              <input id='pass' className='w-full max-w-[260px] min-w-[183px] h-[36px] pt-[9px] pb-[7px] pl-[8px] text-white text-[12px] focus:outline-none' type="password" placeholder='Password' 
+              value={password}
+              onChange={(e)=>setPassword(e.target.value)}/>
             </div>
             <div>
 
               {/* login button */}
-              <button className='w-[270px] h-[32px] bg-[#4a5df9] font-semibold text-[14px] text-white rounded-md mt-2'> Log in</button>
+              <button className='w-full max-w-[270px] min-w-[187px] h-[32px] bg-[#4a5df9] font-semibold text-[14px] text-white rounded-md mt-2'
+              onClick={isLoggedin}> 
+              Log in </button>
             </div>
 
             {/* additional options */}
 
-            <div className='flex  justify-center items-center w-[270px] h-[15px] mt-4 gap-4'>
+            <div className='flex  justify-center items-center w-full max-w-[270px] min-w-[187px] h-[15px] mt-4 gap-4'>
             <div className='w-[107px] h-[1px] bg-[#262626]'> </div>
             <div className=' font-semibold text-white text-[13px]'>OR</div>
             <div className='w-[107px] h-[1px] bg-[#262626]'> </div>
             </div>
 
             {/* login with facebook */}
-            <div className='flex justify-center w-[270px] h-[20px] mt-4'>
+            <div className='flex justify-center max-w-[270px] min-w-[187px] w-full h-[20px] mt-4'>
               <div className='mr-[4px] px-[4px]'>
                 <img className='w-[20px] h-[20px]  bg-[#0095f6] rounded-full' src="Facebook.png" alt="" />
               </div>
@@ -68,7 +92,7 @@ function App() {
 
         </div>
         {/* Sign up */}
-        <div className='flex justify-center w-[350px] h-[61px] py-[10px] text-[14px]'>
+        <div className='flex justify-center w-full min-w-[250px] max-w-[350px] h-[61px] py-[10px] text-[14px]'>
           <span className=' flex justify-center w-[236px] h-[41px] text-white  text-[14px]'>
              Don't have an account? <p className='text-[#708dff] text-[14px] ml-1'>Sign up</p>
           </span>
@@ -81,17 +105,20 @@ function App() {
       </main>
 
       {/* footer */}
-      <footer className='px-[16px]'>
-        <div className='mb-[52px] h-[83px]'>
-          <div className='flex justify-center w-[1273px] text-white text-[12px] gap-4'>
-          <span>Meta</span> <span>About</span> <span>Blog</span> <span>Jobs</span> <span>Help</span> <span>API</span>
-          <span>Privacy</span> <span>Terms</span> <span>Locations</span> <span>Instagram Lite</span> <span>Meta AI</span> 
-          <span>Meta AI articles</span> <span>Threads</span> <span>Contact uploading and non-users</span> <span>Meta Verified</span>
+      <footer className=' px-[16px] w-full min-w-[250px]'>
+        <div className=' mb-[52px] h-[83px]'>
+          <div className='flex flex-wrap justify-center w-full max-w-[1273px] text-white text-[12px] gap-2 sm:gap-4 mx-auto pb-1'>
+          <span className="whitespace-nowrap">Meta</span> 
+          <span className="whitespace-nowrap">About</span> 
+          <span className="whitespace-nowrap">Blog</span> 
+          <span className="whitespace-nowrap">Jobs</span> <span className="whitespace-nowrap">Help</span> <span className="whitespace-nowrap">API</span>
+          <span className="whitespace-nowrap">Privacy</span> <span className="whitespace-nowrap">Terms</span> <span className="whitespace-nowrap">Locations</span> <span className="whitespace-nowrap">Instagram Lite</span> <span className="whitespace-nowrap">Meta AI</span> 
+          <span className="whitespace-nowrap">Meta AI articles</span> <span className="whitespace-nowrap">Threads</span> <span className="whitespace-nowrap">Contact uploading and non-users</span> <span className="whitespace-nowrap">Meta Verified</span>
         </div>
 
-        <div className='flex justify-center text-white text-[12px] my-[12px]'>
-          <span> English (UK)</span>
-          <span> 2025 Instagram from Meta</span>
+        <div className='flex  flex-wrap justify-center w-full max-w-[1273px] text-white text-[12px] my-[12px]'>
+          <span className="whitespace-nowrap"> English (UK)</span>
+          <span className="whitespace-nowrap"> 2025 Instagram from Meta</span>
         </div>
 
 
