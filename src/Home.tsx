7eed756593fcf0f0616c1a5b65850ee78e1c590a
Deepak Kumar <125119545+deepak-077@users.noreply.github.com>
2025-08-21 23:16:13 +0530

    Updated Home dimensions

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,11 +4,11 @@ import Suggestion from "./Suggestion";
 function Home(){
     return(
         //parent container
-        <div className="flex bg-black">
+        <div className="flex flex-col-reverse md:flex-row bg-black ">
             <Sidebar/>
 
             {/* photos and suggestions container */}
-            <div className="flex max-w-[1061px] w-full min-h-screen bg-black">
+            <div className="flex  max-w-[1226px] justify-center w-full min-h-screen bg-black pt-3.5 md:pt-0">
                 <Middle/>
                 
                 <Suggestion/>
