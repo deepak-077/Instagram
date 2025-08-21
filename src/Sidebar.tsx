56f054e8574ddced6b375689676f3112a9fb98d7
Deepak Kumar <125119545+deepak-077@users.noreply.github.com>
2025-08-21 23:12:04 +0530

    made sidebar responsive

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,9 +1,12 @@
+import './App.css'
+import { useEffect,useState } from 'react';
+
 const menu=[
     {
         img:"home.png",
         title:"Home"
     },
-    {
+    {   //not in small
         img:"search.png",
         title:"Search"
     },
@@ -15,11 +18,11 @@ const menu=[
         img:"reels.png",
         title:"Reels"
     },
-    {
+    {   
         img:"home.png",
         title:"Messages"
     },
-    {
+    {   //not in small
         img:"like.png",
         title:"Notifications"
     },
@@ -31,20 +34,40 @@ const menu=[
         img:"mkbhd.jpg",
         title:"Profile"
     },
-    {
+    {   //not in small
         img:"menu.png",
         title:"More"
     },
 ]
 
 function Sidebar(){
+    const [isSmall,setIsSmall] =useState(false);
+
+    useEffect(()=>{
+        const screensize=()=>{
+            setIsSmall(window.innerWidth<768);
+        }
+        screensize();
+
+        window.addEventListener('resize',screensize);
+        return ()=>window.removeEventListener('resize',screensize);
+    },[])
+
+    const visibleMenu=isSmall ? menu.filter(
+        (item) =>
+            item.title!=='Search' &&
+            item.title!=='Notifications'&&
+            item.title!=='More'
+        
+    ):menu
+
     return(
         <>
         {/* sidebar */}
-            <div className="max-w-[240px] w-full bg-black flex flex-col items-center border-r-1 border-gray-800">
+            <div className="w-screen md:max-w-[72px] xl:max-w-[240px] w-full bg-black flex flex-row md:flex-col items-center border-r-1 border-gray-800">
 
-                <div className="flex justify-center w-full max-w-[220px] h-[92px]">
-                    <div className="flex justify-center w-full max-w-[220px] h-[73px] mb-[19px] pt-[25px] px-[12px] pb-[16px]">
+                <div className="flex justify-center w-full max-w-[48px] h-[96px] xl:max-w-[220px] xl:h-[92px] hidden md:block">
+                    <div className="flex justify-center w-full  mt-3 xl:mt-0 xl:pt-[25px]  pb-[23px] xl:pb-[16px] max-w-[48px] h-[92px] xl:max-w-[220px] xl:h-[73px] xl:mb-[19px] xl:px-[12px]">
                         <div className="w-full max-w-[196px] h-[37px]">
                             <img className="rounded-full w-full max-w-[103px] h-[29px]" src="Instagram white.svg" alt="" />
                         </div>
@@ -53,17 +76,16 @@ function Sidebar(){
 
                 {/* menu options*/}
             
-                <div className="w-full max-w-[220px] h-[52px]">
-                            {menu.map((item,index) =>(
-                        <div className="flex w-full max-w-[220px] h-[48px] my-0.5 p-3">
+                <div className="flex flex-row justify-around md:justify-normal md:flex-col w-full md:max-w-[48px] xl:max-w-[220px] h-[52px] ">
+                    {visibleMenu.map((item,index) =>(
+                        <div key={index} className="flex justify-center w-full max-w-[48px] h-[52px] xl:max-w-[220px] xl:h-[48px] md:my-0.5 p-3">
 
                             <img className="w-full max-w-[24px] h-[24px]" src={`/Instagram_files/${item.img}`} alt="" />
-                            <div className="flex items-center w-full max-w-[62px] h-[24px] text-white"> 
+                            <div className="flex items-center w-full max-w-[62px] h-[24px] text-white hidden xl:block"> 
                                 <span className="pl-4">{item.title}</span>
                             </div>
     
                         </div>
-
                     ))}
                     
                 </div>
